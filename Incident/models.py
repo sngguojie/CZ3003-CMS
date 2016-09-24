0c90a7ce485966d6215576631120b784e95610e4
sngguojie <sngguojie@gmail.com>
2016-09-25 02:47:15 +0800

    Melvyn Part

--- Incident/models.py
+++ Incident/models.py
@@ -3,12 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-
 class Incident(models.Model):
-    activation_date = models.DateField()
-    deactivation_date = models.DateField()
+    activation_time = models.DateField()
+    deactivation_time = models.DateField()
     description = models.TextField()
-    incident_summary = models.ForeignKey(IncidentSummary, on_delete=models.CASCADE)
-
+    
     def __unicode__(self):              # __unicode__ on Python 2
         return self.description
--- IncidentSummary/models.py
+++ IncidentSummary/models.py
@@ -1,12 +1,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class IncidentSummary(models.Model):
     datetime = models.DateField()
     description = models.TextField()
+    creator = models.ForeignKey(User)
     
     def __unicode__(self):              # __unicode__ on Python 2
         return self.description
