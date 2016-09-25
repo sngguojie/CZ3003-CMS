d703a5f384790020c0e6c053b5784a49731ca56e
jiaqi1993 <xiaojia1993@gmail.com>
2016-09-25 10:06:43 +0800

    JiaQi Commit 2

--- Agency/models.py
+++ Agency/models.py
@@ -3,9 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-agency_id=models.AutoField(primary_key=True)
-name=models.TextField()
-description=models.TextField()
-sms_contact_no=models.CharField(max_length=8)
-def __unicode__(self):
-	return self.description
+class Agency(models.Model):
+	agency_id=models.AutoField(primary_key=True)
+	name=models.TextField()
+	description=models.TextField()
+	sms_contact_no=models.CharField(max_length=8)
+	def __unicode__(self):
+		return self.description
