9e8d80f36d8f7c9574c80b2d0591a65d69bde018
sngguojie <sngguojie@gmail.com>
2016-09-26 22:54:40 +0800

    incident and summary urls views

--- CZ3003CMS/settings.py
+++ CZ3003CMS/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello',
-    'Incident',
-    'IncidentSummary',
+    'CMSIncident',
+    'CMSIncidentSummary',
     'IncidentCallReport',
     'Agency'
 )
