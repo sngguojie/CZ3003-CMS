5d467ec6d324de1627f6c01482b8dd44b5cbfade
Chen Chin Jieh <chinjieh@gmail.com>
2016-09-27 17:50:15 +0800

    Fix broken migration dependencies

--- CMSIncidentSummary/migrations/0002_incidentsummary_creator.py
+++ CMSIncidentSummary/migrations/0002_incidentsummary_creator.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('IncidentSummary', '0001_initial'),
+        ('CMSIncidentSummary', '0001_initial'),
     ]
 
     operations = [
