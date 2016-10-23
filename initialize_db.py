264b3ee7ca1616261defed882a13fcd057a19fa7
sngguojie <sngguojie@gmail.com>
2016-10-23 17:32:19 +0800

    reverted the unnecessary change of spaces to tabs in initialize_db.py

--- initialize_db.py
+++ initialize_db.py
@@ -9,27 +9,25 @@ from CMSStatus.models import CMSStatus
 
 # Initialize group names
 for GRP_NAME in GRP_NAMES:
-    has_grp = len(Group.objects.filter(name=GRP_NAME)) >= 1
-    if not has_grp:
-        Group.objects.create(name=GRP_NAME)
+	has_grp = len(Group.objects.filter(name=GRP_NAME)) >= 1
+	if not has_grp:
+		Group.objects.create(name=GRP_NAME)
 
 # Initialize users
 for user in USERS:
-    get_user = User.objects.filter(username=user['username'])
-    if not (len(get_user) == 0):
-        continue
-    cur_user = User.objects.create_user(username=user['username'], email=user['email'], password=user['password'])
-    groups_str = user['groups']
-    success, groups = to_groups(groups_str)
-    if not success:
-        logger.debug(groups)
-        continue
-    for group in groups:
-        cur_user.groups.add(group)
-        cur_user.save()
+	get_user = User.objects.filter(username=user['username'])
+	if not (len(get_user) == 0):
+		continue
+	cur_user = User.objects.create_user(username=user['username'], email=user['email'], password=user['password'])
+	groups_str = user['groups']
+	success, groups = to_groups(groups_str)
+	if not success:
+		logger.debug(groups)
+		continue
+	for group in groups:
+		cur_user.groups.add(group)
+		cur_user.save()
 
 # Initialize CMSStatus
 if len(CMSStatus.objects.all()) == 0:
-    CMSStatus.objects.create()
-
-
+	CMSStatus.objects.create()
