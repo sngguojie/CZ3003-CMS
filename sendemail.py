a6a6a763789fbb28a55807a6c5cfa2a340764d22
jiaqi1993 <xiaojia1993@gmail.com>
2016-10-26 22:34:39 +0800

    Update changes to sendemail.py

--- sendemail.py
+++ sendemail.py
@@ -1,15 +1,16 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+import requests
 
 sched = BlockingScheduler()
 count=0
 
-@sched.scheduled_job('interval', minutes=1/60)
+@sched.scheduled_job('interval', minutes=1)
 def timed_job():
     print('This job is run every one minutes.')
     global count, sched
     print "Count is %d !" % (count) 
     count=count+1
-    if count==3:
-       sched.shutdown()
+    url = "http://localhost:5000/emailApp/create"
+    r=requests.post(url, data)
 
 sched.start()
