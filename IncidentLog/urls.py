28848185e949f8d375e771b42b2eedffd5563f01
Chin Jieh <chinjieh@gmail.com>
2016-11-02 02:52:57 +0000

    Remove deprecated urls and files

--- IncidentLog/urls.py
+++ IncidentLog/urls.py
@@ -1,8 +1,5 @@
 from django.conf.urls import url
 import views
-import views_deprec
 
 urlpatterns = [
-    # Deprecated, remove after client has been updated
-    url(r'^create/$', views_deprec.create ,name='create'),
 ]
