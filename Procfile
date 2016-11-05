dd6d07dc9d43ab327ced829bcf216f35f2c97210
jiaqi1993 <xiaojia1993@gmail.com>
2016-11-05 16:54:56 +0800

    Commit Files to Start Email Service on Server Side

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 release: python manage.py migrate
 web: gunicorn CZ3003CMS.wsgi --log-file -
+email_scheduler: python email_scheduler.py
--- emailApp/views.py
+++ emailApp/views.py
@@ -6,6 +6,8 @@ from django.views.decorators.http import require_GET, require_POST
 from CMSStatus.models import CMSStatus
 from django.core.mail import EmailMessage
 from datetime import datetime, timedelta
+from IncidentLog.models import IncidentLog
+from Incident.models import Incident
 
 from common import util, commonHttp, datetime_util
 import logging
@@ -19,35 +21,49 @@ expectedAttr = {
 	'BODY': "body",
 }
 
+cmsexpectedAttr = {
+	'ACTIVE': "active"
+}
+
 @require_POST
 @csrf_exempt
-def create(request):
-	
+def create(request):	
 	json_obj = commonHttp.get_json(request.body)
 
-	req_attrs = [
-		expectedAttr["TO"], 
-		expectedAttr["TITLE"], 
-		expectedAttr["BODY"]
-	]
-	commonHttp.check_keys(json_obj, req_attrs)
 
  	try:
 	 	cms = CMSStatus.objects.get(id=1)
-	 	cms.active = json_obj.get("active")
-	 	cms.last_sent = json_obj.get("last_sent")
+	 	all_incident_logs = IncidentLog.objects.all()
+	 	contentStr=''
+	 	currentPointer=-1
+	 	for incident_logs in reversed(all_incident_logs):
 
-	 	time_exceeds_interval = (datetime_util.sgt_now() - cms.last_sent) > timedelta(minutes = 30)
-	 	if (time_exceeds_interval and cms.active):
-	 		email=EmailMessage(json_obj[expectedAttr["TITLE"]], json_obj[expectedAttr["BODY"]], to=[json_obj[expectedAttr["TO"]]])
+	 		incident = Incident.objects.get(id=incident_logs.incident_id)
+	 		if currentPointer!=incident_logs.incident_id:
+	 			contentStr= contentStr + '\nIncident ID and Type: ' + str(incident_logs.incident_id) + ' ' + incident.incident_type + '\nDescription: ' + incident_logs.description + '\nDateTime: ' + str(incident_logs.datetime) +'\n'
+	 			currentPointer=incident_logs.incident_id
+	 		else:
+	 			contentStr= contentStr + 'Description: ' + incident_logs.description + '\nDateTime: ' + str(incident_logs.datetime) +'\n'
+
+		time_difference=datetime_util.sgt_now() - cms.last_sent
+	 	if time_difference > timedelta(minutes=30):
+	 		email=EmailMessage('Incident Report Generated ' + str(datetime_util.sgt_now()), contentStr, to=['jqlee93@gmail.com'])
 			email.send()
+			cms.last_sent=datetime_util.sgt_now()
+			commonHttp.save_model_obj(cms)
+
+
 		response = JsonResponse({
-		 		"active" : cms.active,
-		 		"success" : True,
-		 		})
+		 	cmsexpectedAttr["ACTIVE"] : cms.active,
+		 	"cms_last_sent": cms.last_sent,
+		 	"success" : True,
+		 	})
 		return response
  	except Exception as e:
 		response = JsonResponse({
+			cmsexpectedAttr["ACTIVE"] : cms.active,
+			"cms_last_sent": cms.last_sent,
 		 	"success" : False,
+		 	"exception": e,
 		 	})
  		return response
